b7d9ef8dadb2920e839b11789adfeaa45fa2d522
ArunawayNERD <jpweb2@hotmail.com>
2014-10-31 22:19:35 -0400

    finsihed logging engine

--- Logging/LoggingEngine.php
+++ Logging/LoggingEngine.php
@@ -19,38 +19,97 @@ class LoggingEngine
 
     public function logStockDateUpdate()
     {
+        $msg = "Stock Data updated";
 
+        $this->fileWriter->logMessage($msg);
     }
 
-    public function logWhatIfScenario()
+    public function logWhatIfScenario($strUser)
     {
+        $msg = (String)$strUser . " created a What-If Scenario.";
 
+        $this->fileWriter->logMessage($msg);
     }
 
     public function logUserLogin($strUser, $boolSuccessOrFail)
     {
+        $msg = (String)$strUser;
 
+        if($boolSuccessOrFail === true)
+               $msg = $msg . " successfully logged into the system. ";
+        else
+                $msg = $msg . " failed to log into the system. ";
+
+        $this->fileWriter->logMessage($msg);
     }
 
-    public function logUserCreation($strUser, $boolSuccessOrFail)
+    public function logUserCreation($strIP, $strUser, $boolSuccessOrFail)
     {
+        $msg = (String)$strIP;
 
+        if($boolSuccessOrFail === true)
+            $msg = $msg . " successfully created a new account " . (String)$strUser;
+        else
+            $msg = $msg . " failed to create a new account.";
+
+        $this->fileWriter->logMessage($msg);
     }
 
 
     public function logPortActivity($strUser, $boolSuccessOrFail)
     {
+        $msg = (String)$strUser;
 
+        if($boolSuccessOrFail === true)
+            $msg = $msg . " successfully created a new portfolio.";
+        else
+            $msg = $msg . " failed to create a new portfolio.";
+
+        $this->fileWriter->logMessage($msg);
     }
 
-    public function logTransActivity($strUser, $booBuyOrSell, $strItems, $boolSuccessOrFail)
+    public function logTransActivity($strUser, $boolSuccessOrFail, $boolBuyOrSell, $strItems)
     {
+        $msg = (String)$strUser;
 
+        if($boolSuccessOrFail === true)
+        {
+            $msg = $msg . " successfully ";
+
+            if($boolBuyOrSell === true)
+                $msg = $msg . "bought ";
+            else
+                $msg = $msg . "sold ";
+        }
+        else
+        {
+            $msg = $msg . " failed to ";
+
+            if ($boolBuyOrSell === true)
+                $msg = $msg . "buy ";
+            else
+                $msg = $msg . "sell ";
+        }
+
+        $msg = $msg . (String)$strItems;
+
+        $this->fileWriter->logMessage($msg);
     }
 
     public function logCompActivity($strUser, $boolCreateOrDelete)
     {
+        $msg = (String)$strUser;
 
+        if($boolCreateOrDelete === true)
+            $msg = $msg . " created a competition.";
+        else
+            $msg = $msg . " deleted a competition.";
+
+        $this->fileWriter->logMessage($msg);
     }
 
+    public function logMessage($msg)
+    {
+        $this->fileWriter->logMessage($msg);
+    }
 } 
--- Logging/LoggingTester.php
+++ Logging/LoggingTester.php
@@ -1,12 +1,33 @@
 <Html>
     <body>
     <?php
-
         require 'LoggingEngine.php';
 
-       $LoggingEngine = new LoggingEngine();
+        $loggingEngine = new LoggingEngine();
 
-        )
+        $loggingEngine->logMessage("Starting log testing");
+        $loggingEngine->logMessage(" ");
+
+        $loggingEngine->logStockDateUpdate();
+
+        $loggingEngine->logWhatIfScenario("UserJohnny");
+
+        $loggingEngine->logUserLogin("UserJohnny", true);
+        $loggingEngine->logUserLogin("UserJohnny", false);
+
+        $loggingEngine->logUserCreation("127.0.0.1", "UserJohnny", true);
+        $loggingEngine->logUserCreation("127.0.0.1", "UserJohnny", false);
+
+        $loggingEngine->logPortActivity("UserJohnny", true);
+        $loggingEngine->logPortActivity("UserJohnny", false);
+
+        $loggingEngine->logTransActivity("UserJohnny", true, true, "1 Google Share for 1 million dollars");
+        $loggingEngine->logTransActivity("UserJohnny", true, false, "1 Google Share for 1 million dollars");
+        $loggingEngine->logTransActivity("UserJohnny", false, true, "1 Google Share for 1 million dollars");
+        $loggingEngine->logTransActivity("UserJohnny", false, false, "1 Google Share for 1 million dollars");
+
+        $loggingEngine->logCompActivity("UserJohnny", true);
+        $loggingEngine->logCompActivity("UserJohnny", false);
 
     ?>
     </body>
