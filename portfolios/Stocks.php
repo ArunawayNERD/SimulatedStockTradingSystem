2814a28363316916e5e71f357a199ad113e9caad
SE Group ug2 <ssts@pluto.hood.edu>
2014-11-19 12:15:44 -0500

    created a php object for stocks

---
+++ portfolios/Stocks.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* implements a stock holding the stock name and the number of shares owned
+* author: John Pigott
+*/
+
+class Stock
+{
+	private $stockName = "";
+	private $numShares = 0;
+
+	public function __construct($name, $num)
+	{
+		$this->stockName = $name;
+
+		if($num < 0)
+			throw new InvalidArgumentException("Number of shares must be greater than or equal to 0. Input was: " . $num);
+	}
+}
